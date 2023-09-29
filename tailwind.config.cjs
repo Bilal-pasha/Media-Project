2f1e5436637aef74857c6213eb759ad143badafa
Bilal-Pasha <billopasho56@gmail.com>
2023-09-29 12:48:11 +0500

    updated

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 content: [
-		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
-		'./node_modules/flowbite/**/*.js'
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'
 	],	theme: {
 		extend: {},
 		backgroundImage:{
@@ -35,8 +34,6 @@ content: [
 			// => @media (min-width: 1536px) { ... }
 		  } 
 	},
-	plugins: [
-        require('flowbite/plugin')
-    ]
+	
 }
 
