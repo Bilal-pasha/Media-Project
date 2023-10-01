c0a486d3c07a18982e8f036c339fea3543d89735
Bilalpasha <billopasho56@gmail.com>
2023-10-01 16:56:28 +0500

    updated-sunday-work

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,39 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-content: [
+	content: [
 		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'
-	],	theme: {
+	], theme: {
 		extend: {},
-		backgroundImage:{
+		backgroundImage: {
 			"gradient": 'linear-gradient(109.6deg, rgb(245, 239, 249) 30.1%, rgb(207, 211, 236) 100.2%)'
 		},
-		colors:{
-			'blue' : '#1E90FF',
-			'white' : 'white',
-			'gray' : 'gray',
-			'skyblue' : '#6CB4EE',
+		colors: {
+			'blue': '#1E90FF',
+			'white': 'white',
+			'gray': 'gray',
+			'skyblue': '#6CB4EE',
 			'black': 'black',
-			'semiblack' : '#0000007d',
-			'lightWhite' : '#e9e1e1',
-			 'red' : 'red',
+			'semiblack': '#0000007d',
+			'lightWhite': '#e9e1e1',
+			'red': 'red',
 		},
 		screens: {
 			'sm': '300px',
 			// => @media (min-width: 640px) { ... }
-	  
+
 			'md': '760px',
 			// => @media (min-width: 768px) { ... }
-	  
+
 			'lg': '1340px',
 			// => @media (min-width: 1024px) { ... }
-	  
+
 			'xl': '1480px',
 			// => @media (min-width: 1280px) { ... }
-	  
+
 			'2xl': '1536px',
 			// => @media (min-width: 1536px) { ... }
-		  } 
+		}
 	},
-	
+
 }
 
