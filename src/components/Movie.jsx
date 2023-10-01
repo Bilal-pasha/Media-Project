74758d2e574b69eea38c6c07d4d4388190e6f574
Bilalpasha <billopasho56@gmail.com>
2023-10-01 09:51:12 +0500

    updated-01-oct

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -4,7 +4,7 @@ import Moviebg from "../assets/videoBg.mp4";
 // import { useState, useEffect } from "@astrojs/react";
 
 export default function Movie() {
-  const [text, setText] = useState(" ");
+  const [text, setText] = useState("");
   const [text2, setText2] = useState("");
   let [currentIndex, setCurrentIndex] = useState(0);
   let [currentIndex2, setCurrentIndex2] = useState(0);
@@ -44,7 +44,6 @@ export default function Movie() {
   return (
     <Fragment>
       <div className="relative w-full lg:h-[80vh] md:h-[60vh] sm:h-[50vh]">
-        {/* <div className="absolute top-0 left-0 w-full h-full bg-[rgba(0,0,0,0.5)]"></div> */}
         <video
           autoPlay={true}
           loop={true}
@@ -54,7 +53,7 @@ export default function Movie() {
         >
           <source src={Moviebg} type="video/mp4" />
         </video>
-        <div className="absolute text-white font-base justify-end items-center lg:py-20 sm:py-16 w-full h-full flex flex-col lg:top-0 lg:left-0 lg:px-[100px] sm:px-[50px] max-w-[100%] lg:space-y-5 sm:space-y-2">
+        <div className="absolute text-white font-base justify-end items-center lg:py-20 sm:py-16 w-full h-full flex flex-col lg:top-0 lg:left-0 lg:px-[100px] sm:px-[50px] max-w-[100%] lg:space-y-5 sm:space-y-2 bg-semiblack">
           <p className="lg:text-3xl sm:text-xl font-[Poppins] font-black">
             {text}
           </p>
@@ -62,24 +61,6 @@ export default function Movie() {
             {text2}
           </p>
         </div>
-        {/* <div className="max-w-[40rem] space-y-2">
-            <p className="lg:text-2xl md:text-xl sm:text-base">
-              Welcome to Max Solution
-            </p>
-            <h2 className="lg:text-5xl md:text-3xl sm:text-2xl ">
-              <span className="text-blue font-semibold">M</span>edia{" "}
-              <span className="text-blue font-semibold">A</span>dvisory E
-              <span className="text-blue font-semibold">X</span>perts
-            </h2>
-            <p className="lg:text-xl sm:text-sm">
-              {" "}
-              Media Advisory Experts is an innovative media advisory company
-              offering strategic communications , network television - level
-              video production and targeted media and journalism training to
-              corporate, government and individual clients across the United
-              States and internationally
-            </p>
-          </div> */}
       </div>
     </Fragment>
   );
--- src/components/Testimonials .jsx
+++ src/components/Testimonials .jsx
@@ -13,27 +13,45 @@ const firebaseConfig = {
     messagingSenderId: "690324972945",
     appId: "1:690324972945:web:d6c8b66ff2cffd3745e630",
     measurementId: "G-7PRP65ENKB"
-};
+}
 
+const responsive = {
+    desktop: {
+        breakpoint: { max: 3000, min: 1200 },
+        items: 3,
+        slidesToSlide: 1 // optional, default to 1.
+    },
+    tablet: {
+        breakpoint: { max: 1200, min: 768 },
+        items: 2,
+        slidesToSlide: 1 // optional, default to 1.
+    },
+    mobile: {
+        breakpoint: { max: 767, min: 464 },
+        items: 1,
+        slidesToSlide: 1 // optional, default to 1.
+    }
+};
 
 
 const Testinomials = () => {
     const [data, setData] = useState([])
+
     initializeApp(firebaseConfig);
     useEffect(() => {
-
         const db = getFirestore()
-        const colRef = collection(db, 'Testinomials')
-        getDocs(colRef).then((snap) => {
-            const images = snap.docs.map((doc) => {
+        const colRef = collection(db, 'testimonials')
+        getDocs(colRef).then((review) => {
+            const reviews = review.docs.map((doc) => {
+                // console.log(data)
                 return { ...doc.data(), id: doc.id }
             })
-            setData(images.sort((a, b) => a.Order - b.Order));
+            setData(reviews.sort((a, b) => a.Order - b.Order));
         }).catch(err => {
             console.log(err.message)
         })
     }, [])
-    console.log(data)
+    // console.log(data)
     return (
 
         <section className="bg-[#f1f1f1] lg:my-20">
@@ -48,22 +66,21 @@ const Testinomials = () => {
                     autoPlay={true}
                     swipeable={true}
                     draggable={true}
-                    showDots={true}
+                    showDots={false}
                     infinite={true}
                     partialVisible={false}
                     dotListClass="custom-dot-list-style"
                 >
-                    {sliderImageUrl.map((data, index) => {
+                    {data.map((data, index) => {
                         return (
                             <>
-                                <div className="px-5 py-8 bg-white mx-5 rounded-lg my-20" key={index}>
+                                <div className="max-h-[40vh] px-5 py-8 bg-white mx-5 rounded-lg my-20" key={index}>
                                     <p>{data.Review}</p>
-                                    <div className="flex justify-around py-5">
+                                    <div className="flex justify-start py-5">
                                         <div>
-                                            <h2 className="font-semibold">{data.ClientName}</h2>
-                                            <p>{data.ClientOrganization}</p>
+                                            <h2 className="font-[Poppins] text-lg">{data.Client}</h2>
+                                            <p className="">{data.ClientOrganization}</p>
                                         </div>
-                                        {/* <img src={data.imageUrl} alt="" /> */}
                                     </div>
                                 </div>
                             </>
@@ -75,53 +92,3 @@ const Testinomials = () => {
     );
 };
 export default Testinomials;
-const sliderImageUrl = [
-    //First image url
-    {
-        Review: "“The Creativess Studio has the ability to simplify complex problems into accessible and easy-to-use solutions. A must-have for any fast-growing team and product.",
-        ClientName: "Colin Deblonde",
-        ClientOrganization: "Co-Founder & CEO",
-        // imageUrl: Client1 
-    },
-    {
-        Review: "“The Creativess Studio has the ability to simplify complex problems into accessible and easy-to-use solutions. A must-have for any fast-growing team and product.",
-        ClientName: "Colin Deblonde",
-        ClientOrganization: "Co-Founder & CEO",
-        // imageUrl: Client2 
-    },
-    {
-        Review: "“The Creativess Studio has the ability to simplify complex problems into accessible and easy-to-use solutions. A must-have for any fast-growing team and product.",
-        ClientName: "Colin Deblonde",
-        ClientOrganization: "Co-Founder & CEO",
-        // imageUrl: Client3 
-    },
-    {
-        Review: "“The Creativess Studio has the ability to simplify complex problems into accessible and easy-to-use solutions. A must-have for any fast-growing team and product.",
-        ClientName: "Colin Deblonde",
-        ClientOrganization: "Co-Founder & CEO",
-        // imageUrl: Client4 
-    }, {
-        Review: "“The Creativess Studio has the ability to simplify complex problems into accessible and easy-to-use solutions. A must-have for any fast-growing team and product.",
-        ClientName: "Colin Deblonde",
-        ClientOrganization: "Co-Founder & CEO",
-        // imageUrl: Client1 
-    }
-];
-
-const responsive = {
-    desktop: {
-        breakpoint: { max: 3000, min: 1200 },
-        items: 4,
-        slidesToSlide: 4 // optional, default to 1.
-    },
-    tablet: {
-        breakpoint: { max: 1200, min: 768 },
-        items: 2,
-        slidesToSlide: 3 // optional, default to 1.
-    },
-    mobile: {
-        breakpoint: { max: 767, min: 464 },
-        items: 1,
-        slidesToSlide: 1 // optional, default to 1.
-    }
-};
