c27c9b30c5fb06ccc6a6e4528d816a1642c2c99a
zaidahmed <zaid.ahmed@techwards.co>
2023-10-03 17:57:14 +0500

    feat(MARK-000): testimonial feature completed

---
+++ src/pages/Contact/FormContact.tsx
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+const FormContact = () => {
+  const [data, setData] = useState('');
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    console.log('kkkk', event);
+  };
+  return (
+    <form
+      onSubmit={(e) => handleSubmit(e)}
+      className="container mx-auto w-full max-w-lg sm:px-10 lg:px-0"
+    >
+      <div className="flex flex-wrap -mx-3 mb-6">
+        <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+          <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+            First Name
+          </label>
+          <input
+            required
+            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+            id="grid-first-name"
+            type="text"
+            placeholder="Jane"
+            onChange={(e) => setData(e.target.value)}
+          />
+        </div>
+        <div className="w-full md:w-1/2 px-3">
+          <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+            Last Name
+          </label>
+          <input
+            required
+            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            id="grid-last-name"
+            type="text"
+            placeholder="Doe"
+          />
+        </div>
+      </div>
+      <div className="flex flex-wrap -mx-3 mb-6">
+        <div className="w-full px-3">
+          <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+            E-mail
+          </label>
+          <input
+            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            id="email"
+            type="email"
+          />
+        </div>
+      </div>
+      <div className="flex flex-wrap -mx-3 mb-6">
+        <div className="w-full px-3">
+          <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+            Message
+          </label>
+          <textarea
+            required
+            className="no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none"
+            id="message"
+          ></textarea>
+        </div>
+      </div>
+      <div className="md:flex md:items-center">
+        <div className="md:w-1/3">
+          <button
+            className="shadow bg-teal-400 hover:bg-teal-400 focus:shadow-outline focus:outline-none text-white bg-blue font-bold py-2 px-4 rounded"
+            type="submit"
+          >
+            Send
+          </button>
+        </div>
+        <div className="md:w-2/3"></div>
+      </div>
+    </form>
+  );
+};
+export default FormContact;
