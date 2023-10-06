16e9a9d2fa7b21dfc6345c120a6adbe036e767d6
samiullah-farooqui <samiullah.farooqui@techwards.co>
2023-10-06 19:56:59 +0500

    fixed book a free consultation on home page

--- src/components/Testimonials/components/ContentLoop.jsx
+++ src/components/Testimonials/components/ContentLoop.jsx
@@ -3,10 +3,10 @@ import React from "react";
 export default function ContentLoop({ data }) {
   return (
     <div className="lg:space-y-10 sm:space-y-4 sm:h-[50vh]">
-      <i class="leading-5 text-xl text-justify">{data.Review}</i>
-      <div class="flex flex-col items-end space-y-3">
-        <h3 class="text-2xl font-[Poppins]">{data.Client}</h3>
-        <span class="leading-5 text-xl">{`${data.ClientOrganization}`}</span>
+      <i className="leading-5 text-xl text-justify">{data.Review}</i>
+      <div className="flex flex-col items-end space-y-3">
+        <h3 className="text-2xl font-[Poppins]">{data.Client}</h3>
+        <span className="leading-5 text-xl">{`${data.ClientOrganization}`}</span>
       </div>
       <div className="flex justify-end">
         {data.Image && <img src={data.Image} width="90" height="50" />}
