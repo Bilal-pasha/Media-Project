95097673fed8280a5f408d319ae0e8d857717730
Bilal-Pasha <billopasho56@gmail.com>
2023-10-13 18:41:48 +0500

    updated-13-10

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -6,7 +6,7 @@ import "./global.css";
 export default function Movie() {
   return (
     <Fragment>
-      <div className="relative w-full lg:h-[60vh] md:h-[60vh] sm:h-[50vh]">
+      <div className="relative w-full lg:h-[60vh] md:h-[60vh] sm:h-[30vh]">
         <video
           autoPlay={true}
           loop={true}
@@ -16,8 +16,8 @@ export default function Movie() {
         >
           <source src={Moviebg} type="video/mp4" />
         </video>
-        <div className="absolute text-white font-base justify-end items-center lg:py-20 sm:py-16 w-full h-full flex flex-col lg:top-0 lg:left-0 lg:px-[100px] sm:px-[50px] max-w-[100%] lg:space-y-5 sm:space-y-2 bg-semiblack">
-          <p className="lg:text-5xl sm:text-3xl sm:text-center border-t-2 border-[#c3c3c3] pt-10 font-[Poppins] font-black duration-[10ms] animated fadeInDown">
+        <div className="absolute text-white font-base justify-end items-center lg:py-20 sm:py-6 w-full h-full flex flex-col lg:top-0 lg:left-0 lg:px-[100px] sm:px-[50px] max-w-[100%] lg:space-y-5 sm:space-y-2 bg-semiblack">
+          <p className="lg:text-5xl sm:text-2xl sm:text-center border-t-2 border-[#c3c3c3] lg:pt-10 sm:pt-3 font-[Poppins] font-black duration-[10ms] animated fadeInDown">
             Welcome to <br />
             MEDIA ADVISORY <span className="text-blue">EXPERTS</span>
           </p>
