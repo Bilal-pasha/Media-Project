da42e2f9557c1076d6123d4d368762784c22423c
samiullah-farooqui <samiullah.farooqui@techwards.co>
2023-10-22 18:03:12 +0500

    cleaned up repo for transfering to marks repo

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -7,13 +7,6 @@ import { getFirestore, collection, getDocs } from 'firebase/firestore'
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBq8-RG6mhrMLiziMwTd1Sv67GbJOi_VrY",
-    authDomain: "composed-world-393911.firebaseapp.com",
-    projectId: "composed-world-393911",
-    storageBucket: "composed-world-393911.appspot.com",
-    messagingSenderId: "690324972945",
-    appId: "1:690324972945:web:d6c8b66ff2cffd3745e630",
-    measurementId: "G-7PRP65ENKB"
 };
 export default function Team() {
     const [data, setData] = useState([])
